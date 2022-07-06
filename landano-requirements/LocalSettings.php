6ee25176c69067909816bcabd89406a14a5fe675
ross-spencer <ross@landano.io>
2022-07-06 21:21:38 +0000

    Customize Wikibase
    Customize Wikibase to satisfy the requirements of
    requrements.landano.io.
    
    * Create various local volumes for ease of backup and maintenance.
    * Placeholder directories for various Wikimedia extensions.
    * NGINX configuration to enable ssl for the site.
    * Miscellaneous other changes by way of configuration.

--- landano-requirements/LocalSettings.php
+++ landano-requirements/LocalSettings.php
@@ -1,11 +1,6 @@
 <?php
 
 /*******************************/
-/* Enable Federated properties */
-/*******************************/
-#$wgWBRepoSettings['federatedPropertiesEnabled'] = true;
-
-/*******************************/
 /* Enables ConfirmEdit Captcha */
 /*******************************/
 #wfLoadExtension( 'ConfirmEdit/QuestyCaptcha' );
@@ -24,3 +19,48 @@
 /* Disable UI error-reporting  */
 /*******************************/
 #ini_set( 'display_errors', 0 );
+
+$wgFavicon = "$wgScriptPath/assets/logo/favicon.ico";
+$wgLogo = "$wgScriptPath/assets/logo/wiki.png";
+
+#$wgForceHTTPS  = true;
+
+/* Doubles as concept URI for WDQS - HTTP ONLY */
+$wgServer = "http://requirements.landano.io";
+
+$wgDisableAnonTalk = true;
+
+$wgGroupPermissions['*']['edit'] = false;
+
+$wgGroupPermissions['*']['createaccount'] = false;
+
+/**** recent changes: 1 year ******/
+$wgRCMaxAge = 365 * 24 * 3600;
+
+$wgSitename = "requirements.landano.io";
+$wgLanguageCode = "en";
+
+// NB. not a variable, but function.
+wfLoadExtension( 'WikibaseLocalMedia' );
+
+$wgEnableUploads = true;
+
+$wgUploadDirectory = "$IP/uploads";
+$wgUploadPath = "$wgScriptPath/uploads";
+
+// File uploads:
+$wgCheckFileExtensions = false;
+$wgMaxUploadSize = 106000000;
+
+wfLoadExtension( 'MobileFrontend' );
+
+
+wfLoadSkin( 'MinervaNeue' );
+$wgMFDefaultSkinClass = 'SkinMinerva';
+$wgMinervaAlwaysShowLanguageButton = true;
+
+# wfLoadSkin( 'Timeless' );
+# $wgDefaultMobileSkin = 'timeless'; // use Timeless skin
+
+# wfLoadSkin( 'Vector' );
+# $wgDefaultMobileSkin = 'vector'; // use Vector skin
