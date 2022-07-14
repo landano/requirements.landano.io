51fcf4eae8c7c087a9e8a6ead257ed79af5b9433
ross-spencer <ross@landano.io>
2022-07-14 15:47:26 +0000

    Comment LocalSettings.php
    Ensure features used are documented. Unused features are
    removed.

--- landano-requirements/LocalSettings.php
+++ landano-requirements/LocalSettings.php
@@ -1,68 +1,70 @@
 <?php
 
 /*******************************/
-/* Enables ConfirmEdit Captcha */
-/*******************************/
-#wfLoadExtension( 'ConfirmEdit/QuestyCaptcha' );
-#$wgCaptchaQuestions = [
-#  'What animal' => 'dog',
-#];
-
-#$wgCaptchaTriggers['edit']          = true;
-#$wgCaptchaTriggers['create']        = true;
-#$wgCaptchaTriggers['createtalk']    = true;
-#$wgCaptchaTriggers['addurl']        = true;
-#$wgCaptchaTriggers['createaccount'] = true;
-#$wgCaptchaTriggers['badlogin']      = true;
-
-/*******************************/
 /* Disable UI error-reporting  */
 /*******************************/
 #ini_set( 'display_errors', 0 );
 
+/*****************************************/
+/* Disable API endpoint error-reporting  */
+/*****************************************/
 #$wgDebugToolbar = true;
 
+/**********************************/
+/* Set logo to local ico and png  */
+/**********************************/
 $wgFavicon = "$wgScriptPath/assets/logo/favicon.ico";
 $wgLogo = "$wgScriptPath/assets/logo/wiki.png";
 
-#$wgForceHTTPS  = true;
-
-/* Doubles as concept URI for WDQS - HTTP ONLY */
+/**********************************************/
+/* Site URL                                   */
+/* NB. Doubles as concept URL for WDQS - HTTP */
+/*     do not change!                         */
+/**********************************************/
 $wgServer = "http://requirements.landano.io";
 
-$wgDisableAnonTalk = true;
-
-$wgGroupPermissions['*']['edit'] = false;
-
-$wgGroupPermissions['*']['createaccount'] = false;
-
-/**** recent changes: 1 year ******/
-$wgRCMaxAge = 365 * 24 * 3600;
-
+/*******************************/
+/* Additional site MD          */
+/*******************************/
 $wgSitename = "requirements.landano.io";
 $wgLanguageCode = "en";
 
-// NB. not a variable, but function.
+/*******************************/
+/* Site protections            */
+/*******************************/
+$wgDisableAnonTalk = true;
+$wgGroupPermissions['*']['edit'] = false;
+$wgGroupPermissions['*']['createaccount'] = false;
+
+/*******************************/
+/* 1 year for recent changes   */
+/*******************************/
+$wgRCMaxAge = 365 * 24 * 3600;
+
+/*******************************/
+/* Local media extension       */
+/*******************************/
+
+// Load Extension function.
 wfLoadExtension( 'WikibaseLocalMedia' );
 
+// Variables
 $wgEnableUploads = true;
-
 $wgUploadDirectory = "$IP/uploads";
 $wgUploadPath = "$wgScriptPath/uploads";
 
-// File uploads:
+// File uploads additional settings.
 $wgCheckFileExtensions = false;
 $wgMaxUploadSize = 106000000;
 
+/*******************************/
+/* Enable Mobile frontend      */
+/*******************************/
+
+// Functions.
 wfLoadExtension( 'MobileFrontend' );
-
-
 wfLoadSkin( 'MinervaNeue' );
+
+// Variables.
 $wgMFDefaultSkinClass = 'SkinMinerva';
 $wgMinervaAlwaysShowLanguageButton = true;
-
-# wfLoadSkin( 'Timeless' );
-# $wgDefaultMobileSkin = 'timeless'; // use Timeless skin
-
-# wfLoadSkin( 'Vector' );
-# $wgDefaultMobileSkin = 'vector'; // use Vector skin
